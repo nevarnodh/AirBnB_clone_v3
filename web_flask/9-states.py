a277013fb65fbb4d789a49331629729961d06f70
nevarnodh <nevillendonji@yahoo.com>
2024-01-04 01:53:57 -0800

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
 """
-from models import storage
+A script that starts a Flask web application:
+"""
+
 from flask import Flask
+from models import storage
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
+@app.route('/states', strict_slashes=False)
+def states_list_route():
     """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
+    List states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id_route(id):
+    """
+    Get a state by id
+    Returns:
+        html: template that lists cities of state sort by name A->Z
+    """
+    state = None
+    for s in storage.all("State").values():
+        if s.id == id:
+            state = s
+            break
+    return render_template("9-states.html", state=state)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
